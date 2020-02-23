f2db07a4608913d1f70d92256028bd2e06383661
Israel <mansion.lagrimas@gmail.com>
2020-02-23 13:48:28 -0600

    Show data on local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 var title, description;
 $(document).ready(function($) {
+	getTotalTasks();
+	getAllTasks();
 	$("#addNewTask").click(function(){
 		title = $("#title").val();
 		description	= $("#description").val();
@@ -18,10 +20,13 @@ $(document).ready(function($) {
 			objTask['id'] = id; 
 			objTask['title'] = title;
 			objTask['description'] = description;
-			localStorage.setItem("task_" + id, JSON.stringify(objTask));
+			objTask['done'] = false;
+			localStorage.setItem(id, JSON.stringify(objTask));
 			console.log(localStorage.length);
 			$("input,textarea").val('');
 			$("#addingTask").modal('hide');
+			getTotalTasks ();
+			getAllTasks();
 		}
 	});	
 
@@ -33,3 +38,32 @@ $(document).ready(function($) {
 		$(this).next('.invalid-feedback').hide();
 	});	
 });
+
+function getTotalTasks (){
+	if (localStorage.length>0) {
+		$("#task-table").show();
+	}else{
+		$("#task-table").hide();
+	}	
+}
+
+function getAllTasks(){
+	list_tasks = [];
+	
+	for (var i = 1; i <= localStorage.length; i++) {
+		var task = JSON.parse(localStorage.getItem(i));
+		var done = task.done ? 'Done' : 'Pending';
+		row = `
+			<tr>
+				<td>${ task.title}</td>
+				<td>${ task.description}</td>
+				<td>${ done }</td>
+				<td>
+					<button class="btn btn-secondary mb-2" id=remove_${task.id}>Remove</button>
+					<button class="btn btn-primary mb-2" id=done_${task.id}>Set as done</button>
+				</td>
+			</tr>
+		`;
+		$("#taskList").append(row);			
+	};
+}
